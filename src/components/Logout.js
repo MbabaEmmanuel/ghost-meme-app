afbc288ab457f50fd9497298166404f403f32a96
Yusuf M <circutrider21@gmail.com>
2021-07-25 18:52:27 -0500

    Minor Bugfix
    Signed-off-by: Yusuf M <circutrider21@gmail.com>

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,11 @@
-// import { useHistory } from "react-router-dom";
-import { createBrowserHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+// import { createBrowserHistory } from "react-router-dom";
 
 function Logout() {
 
     sessionStorage.clear();
-    let history = createBrowserHistory();
-    // history.go(0);
+    // TODO: Investigate into using other methods for reloading the page 
+    let history = useHistory();
     history.push("/");
 
     return (null);

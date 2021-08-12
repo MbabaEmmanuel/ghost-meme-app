aacea92f33ae97ff9a13935364d500b356e004a7
Yusuf M <circutrider21@gmail.com>
2021-08-12 13:48:42 -0500

    chats, memes: Added Support for Base64 Enconding
    Signed-off-by: Yusuf M <circutrider21@gmail.com>

--- src/components/FullConversation.js
+++ src/components/FullConversation.js
@@ -3,9 +3,13 @@ import Meme from './Meme';
 
 function FullConversation() {
     const location = useLocation();
+    console.log(location.pathname);
+
     //passed in memes
     const { memes } = location.state;
     
     return memes.map(m => <Meme data={m} />)
 }
+
 export default FullConversation;
+
